26b3f9cd0650e11cc17f8d122fe98709e6da32d1
David Hwang <asher1225@gmail.com>
2023-07-18 11:22:39 +0900

    first upload

---
+++ utils/index.ts
@@ -0,0 +1,66 @@
+import { CarProps, FilterProps } from '@/types';
+
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+  const headers = {
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
+    'X-RapidAPI-Host': process.env.NEXT_PUBLIC_RAPID_API_HOST
+  };
+
+  const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&fuel_type=${fuel}&limit=${limit}`;
+  // console.log('url', url);
+  const response = await fetch(
+    // `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&fuel_type=${fuel}=&limit=${limit}`,
+      url,
+    {
+      headers: headers
+    }
+  );
+
+  const result = response.json();
+
+  return result;
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL(`https://cdn.imagin.studio/getimage`);
+  const { make, year, model } = car;
+
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || 'whoami');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+};
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+
+  const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+  return newPathName;
+};
+
+
